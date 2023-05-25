6b32c56da8221ae6f4f9ab10fed40e07dab5856b
ordm <ordm67@gmail.com>
2023-05-25 13:12:57 +0300

    Extend ActiveAdmin the before_load way instead of within the engines

--- lib/active_admin/xls/engine.rb
+++ lib/active_admin/xls/engine.rb
@@ -8,17 +8,6 @@ module ActiveAdmin
         if Mime::Type.lookup_by_extension(:xls).nil?
           Mime::Type.register 'application/vnd.ms-excel', :xls
         end
-
-        ActiveAdmin::Views::PaginatedCollection.add_format :xls
-       
-        ActiveSupport.on_load(:active_admin_controller) do
-          ActiveAdmin::ResourceDSL.send :include, ActiveAdmin::Xls::DSL
-          ActiveAdmin::Resource.send :include, ActiveAdmin::Xls::ResourceExtension
-          ActiveAdmin::ResourceController.send(
-            :prepend,
-            ActiveAdmin::Xls::ResourceControllerExtension
-          )
-        end
       end
     end
   end
